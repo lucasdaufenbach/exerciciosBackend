ffff5bc875a73da3a9dc4aa5d5fe2225c87c8f71
Lucas Daufenbach <141446643+lucasdaufenbach@users.noreply.github.com>
2024-05-22 19:44:35 -0300

    Delete Exercicio6 directory

--- Exercicio6/src/main/java/com/example/Exercicio6/Repository/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Exercicio6.Repository;
-import com.example.Exercicio6.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
